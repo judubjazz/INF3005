581a2b0f3b1a5d8dc033eb7f4a400653369ea35a
judubjazz <julienguite3@hotmail.com>
2018-03-13 01:50:44 -0400

    linear gradient

--- tp1/tp1/static/js/jour.js
+++ tp1/tp1/static/js/jour.js
@@ -1,31 +1,26 @@
-
-$(document).ready(()=>{
-    if($('#erreur_400').length >0 ){
+$(document).ready(() => {
+    if ($('#erreur_400').length > 0) {
         showText("#erreur_400", "AUCUNE DONNÉE POUR CE JOUR", 0, 100);
     }
-    if($('#erreur_404').length > 0) {
+    if ($('#erreur_404').length > 0) {
         showText("#erreur_404", "IL N'Y A PAS DE MATRICULE À CE NOM", 0, 100);
     }
 });
-
-
-
-
 
 
 /**
  *construit un url pour visualiser le mois du post
  *
  */
-overview_calendar = ()=>{
-    let pathname = location.href.slice(-18,-10);
-    console.log(pathname);
-    console.log(pathname.slice(0,8));
-   let url = location.protocol + location.hostname +':'+ location.port + pathname;
-   url +='overview/';
-   url +=location.href.slice(-10,-3);
-   url = new URL(url);
-   window.location.href=url;
+overview_calendar = () => {
+    let pathname = location.href.slice(-18, -10);
+    // console.log(pathname);
+    // console.log(pathname.slice(0, 8));
+    let url = location.protocol + location.hostname + ':' + location.port + pathname;
+    url += 'overview/';
+    url += location.href.slice(-10, -3);
+    url = new URL(url);
+    window.location.href = url;
 };
 
 
@@ -34,20 +29,20 @@ overview_calendar = ()=>{
  * @param action : today | tomorrow
  * @param _date: date courrante
  */
-naviguer_jour = (action, _date) =>{
+naviguer_jour = (action, _date) => {
     _date = _date.replace(/[^\w\s-]/gi, '');
     let date = new Date(_date);
-    let url = window.location.href.slice(0,-10);
+    let url = window.location.href.slice(0, -10);
     let now = new Date(date.getTime());
-    if(action === 'yesterday'){
-        let yesterday = new Date (now.setDate(date.getDate() - 1));
-        let day_tostring = yesterday.toISOString().slice(0,10);
-        window.location.href = url+day_tostring;
-    }else if ( action === 'tomorrow'){
-        let tomorrow = new Date (now.setDate(date.getDate() + 1));
-        let day_tostring = tomorrow.toISOString().slice(0,10);
-        window.location.href = url+day_tostring;
-    }else{
+    if (action === 'yesterday') {
+        let yesterday = new Date(now.setDate(date.getDate() - 1));
+        let day_tostring = yesterday.toISOString().slice(0, 10);
+        window.location.href = url + day_tostring;
+    } else if (action === 'tomorrow') {
+        let tomorrow = new Date(now.setDate(date.getDate() + 1));
+        let day_tostring = tomorrow.toISOString().slice(0, 10);
+        window.location.href = url + day_tostring;
+    } else {
         console.log('error action')
     }
 };
@@ -58,21 +53,28 @@ naviguer_jour = (action, _date) =>{
  * @param action : DELETE | UPDATE
  * @param id: le id à modifier dans la bd
  */
-modifier= (action, id) =>{
+modifier = (action, id) => {
     let code_projet = document.getElementById("code_projet" + id).value;
     let matricule = document.getElementById("matricule" + id).value;
     let duree = document.getElementById("duree" + id).value;
-    let dateString =document.getElementById("date" + id).value;
+    let dateString = document.getElementById("date" + id).value;
     let mois = window.location.pathname.slice(-7);
 
-    let object_json= {"id":id,"code_projet": code_projet, "matricule": matricule, "date_publication" :dateString, "duree": duree, "mois": mois};
-     console.log(object_json);
+    let object_json = {
+        "id": id,
+        "code_projet": code_projet,
+        "matricule": matricule,
+        "date_publication": dateString,
+        "duree": duree,
+        "mois": mois
+    };
+    // console.log(object_json);
 
-     if (action === 'supprimer'){
+    if (action === 'supprimer') {
         submit_object_json(object_json, 'DELETE')
-     }else{
+    } else {
         submit_object_json(object_json, 'UPDATE')
-     }
+    }
 
 };
 
@@ -82,30 +84,32 @@ modifier= (action, id) =>{
  * @param object_json : object à soumettre
  * @param action: UPDATE | DELETE
  */
-submit_object_json = (object_json, action)=>{
+submit_object_json = (object_json, action) => {
 
-    let url= new URL(location.protocol + location.hostname +':'+ location.port + '/login');
+    let url = new URL(location.protocol + location.hostname + ':' + location.port + '/login');
     fetch(url, {
-          method:action,
-          headers: {
-              'Accept': '*/*',
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(object_json)
-      })
-          .then(result => {
-              if (result.ok){
-                  //pour reloader apres execution de la function showdiv
+        method: action,
+        headers: {
+            'Accept': '*/*',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(object_json)
+    })
+        .then(result => {
+            if (result.ok) {
+                //pour reloader apres execution de la function showdiv
                 $('#shooting_star').show();
                 window.location.reload()
 
-              }
-              console.log(result);
-           })
-          .catch(error => {
-                  console.log('login():Erreur : ' + error.stack);
-                  alert(error.stack)
-              }
-          )
-          .then((response) => {console.log(response);});
+            }
+            console.log(result);
+        })
+        .catch(error => {
+                console.log('login():Erreur : ' + error.stack);
+                alert(error.stack)
+            }
+        )
+        .then((response) => {
+            console.log(response);
+        });
 };
